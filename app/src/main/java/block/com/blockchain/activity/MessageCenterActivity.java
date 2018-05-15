d21dfc731e89047aaa96e3226a4d6f091612e396
tushun <656715728@qq.com>
2018-05-16 02:07:21 +0800

    资质

---
+++ app/src/main/java/block/com/blockchain/activity/MessageCenterActivity.java
@@ -0,0 +1,14 @@
+package block.com.blockchain.activity;
+
+import block.com.blockchain.R;
+
+/**
+ * Created by Administrator on 2018/5/16.
+ */
+
+public class MessageCenterActivity extends BaseActivity {
+    @Override
+    public void init() {
+setContentView(R.layout.activity_msg_center);
+    }
+}
---
+++ app/src/main/java/block/com/blockchain/adapter/MessageCenterAdapter.java
@@ -0,0 +1,8 @@
+package block.com.blockchain.adapter;
+
+/**
+ * Created by ts on 2018/5/16.
+ */
+
+public class MessageCenterAdapter {
+}
